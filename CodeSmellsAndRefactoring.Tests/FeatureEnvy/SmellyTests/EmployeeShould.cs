35dc3f516e99c13013490ad743fb9383f227c200
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2023-12-09 22:43:45 +0000

    Running through Dan's video

--- CodeSmellsAndRefactoring.Tests/FeatureEnvy/SmellyTests/EmployeeShould.cs
+++ CodeSmellsAndRefactoring.Tests/FeatureEnvy/SmellyTests/EmployeeShould.cs
@@ -19,5 +19,132 @@ namespace CodeSmellsAndRefactoring.Tests.FeatureEnvy.SmellyTests
             Assert.Equal(10, employee.VacationDays);
             Assert.Equal(5, employee.YearsOfService);
         }
+
+        [Theory]
+        [InlineData(-50000)]
+        [InlineData(0)]
+        public void not_be_constructed_with_zero_or_negative_salary(double salary)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Employee("John Doe", "Engineering", salary, 2000, 10, 5));
+        }
+
+        [Fact]
+        public void not_be_constructed_with_negative_years_of_service()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Employee("Jane Doe", "Engineering", 50000, 2000, 10, -5));
+        }
+
+        [Fact]
+        public void not_be_constructed_with_negative_work_hours()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Employee("Jane Doe", "Engineering", 50000, -2000, 10, 5));
+        }
+
+        [Theory]
+        [InlineData(5, 50000, 11500)]
+        [InlineData(6, 50000, 11500)]
+        public void adjusts_the_tax_rate_based_on_years_of_service(
+            int yearsOfService, 
+            double salary,
+            double expectedTaxAmount)
+        {
+            var employee = new Employee("John Doe", "Engineering", salary, 2000, 10, yearsOfService);
+            Assert.Equal(expectedTaxAmount, employee.CalculateTax());
+        }
+        
+        [Fact]
+        public void generate_payslips()
+        {
+            var employee = new Employee("John Doe", "Engineering", 50000, 2000, 10, 5);
+            var expectedPaySlip =
+                $"Name: John Doe\nDepartment: Engineering\nSalary: 50000\nTax: 11500\n";
+            var paySlip = employee.GeneratePaySlip();
+            
+            Assert.Equal(expectedPaySlip, paySlip);
+        }
+
+        [Theory]
+        [InlineData(2050, 9, 4, true)]
+        [InlineData(2000, 9, 4, false)]
+        [InlineData(2050, 10, 4, false)]
+        [InlineData(2050, 9, 3, false)]
+        public void determine_whether_an_employee_is_eligible_for_promotion(
+            int workHours, 
+            int vacationDays,
+            int yearsOfService, 
+            bool expectedEligibility)
+        {
+            var employee = new Employee("John Doe", "Engineering", 50000, workHours, vacationDays, yearsOfService);
+            Assert.Equal(expectedEligibility, employee.IsEligibleForPromotion());
+        }
+        
+        [Theory]
+        [InlineData("Engineering", 50000, 5, 11500)]
+        [InlineData("HR", 60000, 5, 10800)]
+        [InlineData("Sales", 70000, 5, 14000)]
+        [InlineData("Marketing", 80000, 5, 15200)]
+        [InlineData("Unknown", 90000, 5, 18900)]
+        public void calculate_the_correct_tax_for_each_department(
+            string department, 
+            double salary, 
+            int yearsOfService,
+            double expectedTaxAmount)
+        {
+            var employee = new Employee("John Doe", department, salary, 2000, 10, yearsOfService);
+            Assert.Equal(expectedTaxAmount, employee.CalculateTax());
+        }
+
+        [Theory]
+        [InlineData(39, 50000, 0)]
+        [InlineData(40, 50000, 0)]
+        [InlineData(41, 50000, 1875)]
+        public void calculate_the_correct_amount_of_overtime_pay(
+            int workHours, 
+            double salary,
+            double expectedOvertimePay)
+        {
+            var employee = new Employee("John Doe", "Engineering", salary, workHours, 10, 5);
+            Assert.Equal(expectedOvertimePay, employee.CalculateOvertimePay());
+        }
+        
+        [Theory]
+        [InlineData(10, 50000, 25000)]
+        [InlineData(20, 50000, 40000)]
+        public void calculate_the_correct_amount_of_holiday_pay(
+            int vacationDays, 
+            double salary,
+            double expectedVacationPay)
+        {
+            var employee = new Employee("John Doe", "Engineering", salary, 2000, vacationDays, 5);
+            Assert.Equal(expectedVacationPay, employee.CalculateHolidayPay());
+        }
+        
+        [Theory]
+        [InlineData(2100, 8, 5, "Engineering", "Outstanding")]
+        [InlineData(1850, 10, 4, "HR", "Excellent")]
+        [InlineData(1850, 12, 3, "Sales", "Good")]
+        [InlineData(1600, 16, 2, "Engineering", "Needs Improvement")]
+        public void EvaluatePerformance_Returns_Correct_Rating(
+            int workHours, 
+            int vacationDays, 
+            int yearsOfService, 
+            string department, 
+            string expectedRating)
+        {
+            var employee = new Employee("Jane Doe", department, 50000, workHours, vacationDays, yearsOfService);
+            Assert.Equal(expectedRating, employee.EvaluatePerformance());
+        }
+
+        [Fact]
+        public void GeneratePerformanceReport_Returns_Correct_Report()
+        {
+            var employee = new Employee("Jane Doe", "Engineering", 50000, 2100, 8, 4);
+            const string expectedReport =
+                $"Employee: Jane Doe\nDepartment: Engineering\nPerformance: Outstanding\nYears of Service: 4\n";
+            Assert.Equal(expectedReport, employee.GeneratePerformanceReport());
+        }
     }
 }
--- CodeSmellsAndRefactoring/FeatureEnvy/Employee.cs
+++ CodeSmellsAndRefactoring/FeatureEnvy/Employee.cs
@@ -7,16 +7,108 @@
         public double Salary { get; set; }
         public int WorkHours { get; set; }
         public int VacationDays { get; set; }
-        public int YearsOfService { get; set; }
+        public int YearsOfService { get; set; } 
 
         public Employee(string name, string department, double salary, int workHours, int vacationDays, int yearsOfService)
         {
+            if (salary <= 0) throw new ArgumentOutOfRangeException(nameof(salary), "Salary must be greater than zero.");
+            if (yearsOfService < 0) throw new ArgumentOutOfRangeException(nameof(yearsOfService), "Years of service cannot be negative.");
+            if (workHours < 0) throw new ArgumentOutOfRangeException(nameof(workHours), "Work hours cannot be negative.");
+
             Name = name;
             Department = department;
             Salary = salary;
             WorkHours = workHours;
             VacationDays = vacationDays;
             YearsOfService = yearsOfService;
+        }
+
+        private double GetBaseTaxRate()
+        {
+            return Department switch
+            {
+                "Engineering" => 0.25,
+                "HR" => 0.20,
+                "Sales" => 0.22,
+                "Marketing" => 0.21,
+                _ => 0.23
+            };
+        }
+
+        private double GetServiceYearsBonus()
+        {
+            return YearsOfService >= 5 ? 0.02 : 0.0;
+        }
+
+        public double CalculateTax()
+        {
+            var baseTaxRate = GetBaseTaxRate();
+            var serviceYearsBonus = GetServiceYearsBonus();
+
+            return double.Round(Salary * (baseTaxRate - serviceYearsBonus)); 
+        }
+
+        public string GeneratePaySlip()
+        {
+            return $"Name: {Name}\nDepartment: {Department}\nSalary: {Salary}\nTax: {CalculateTax()}\n";
+        }
+
+        public bool IsEligibleForPromotion()
+        {
+            var meetsWorkHourCriteria = WorkHours > 2000;
+            var meetsVacationCriteria = VacationDays < 10;
+            var meetsServiceCriteria = YearsOfService > 3;
+
+            return meetsWorkHourCriteria && meetsVacationCriteria && meetsServiceCriteria;
+        }
+
+        public double CalculateOvertimePay()
+        {
+            if (WorkHours <= 40)
+            {
+                return 0.0;
+            }
+            return (WorkHours - 40) * (Salary / 40) * 1.5;
+        }
+
+        public double CalculateHolidayPay()
+        {
+            var vacationRate = VacationDays > 15 ? 0.8 : 1.0;
+
+            return VacationDays * (Salary / 20) * vacationRate;
+        }
+
+        public string EvaluatePerformance()
+        {
+            var score = 0;
+
+            score += WorkHours > 1800 ? 10 : 0;
+            score -= VacationDays > 12 ? 5 : 0;
+            score += YearsOfService > 3 ? 5 : 0;
+            score += WorkHours > 2000 && Department == "Engineering" ? 10 : 0;
+            score -= VacationDays > 15 ? 5 : 0;
+
+            if (score > 15)
+            {
+                return "Outstanding";
+            }
+
+            if (score > 10)
+            {
+                return "Excellent";
+            }
+
+            if (score > 5)
+            {
+                return "Good";
+            }
+
+            return "Needs Improvement";
+        }
+
+        public string GeneratePerformanceReport()
+        {
+            return $"Employee: {Name}\nDepartment: {Department}\nPerformance: {EvaluatePerformance()}\nYears of Service: {YearsOfService}\n";
         }
     }
 }
